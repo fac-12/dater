3d3acc6b96055bccc5312b7e684be7af93323300
Lucy King <lucy.m.king92@gmail.com>
2017-12-14 23:23:22 +0000

    fixed test

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../src/app');
 
-const endpoints = ['/', '/add', '/lucky', '/result'];
+const endpoints = ['/', '/add', '/lucky', '/result/:tag'];
 endpoints.forEach(endpoint => {
     test('All routes should return status code 200 and text/html', t => {
         request(app)
